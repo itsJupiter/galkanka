1925c3ff5c92a777850dec662c8543f811b91151
itsjupiter <itsjupiter@gmail.com>
2013-06-25 18:52:41 +0800

    v0.001

--- upload_file.php
+++ upload_file.php
@@ -1,8 +1,7 @@
 <?php
-
         echo $_POST['filename'] . " " . $_POST['type'] ." " . $_POST['user'];
-        echo "<form action='upload_file_true.php' method='post'>";
-        echo "<input type='file' name='file' id='file' /> ";
+        echo "<form action='upload_file_true.php' method='post' enctype='multipart/form-data' >";
+        echo "<input type='file' name='file'/> ";
         echo "<br />";
         echo "<input type='submit' name='submit' value='Submit' />";
         echo "</form>";
