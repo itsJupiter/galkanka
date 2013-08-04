3031727996f21d00c32abda05aab05922fd4aa3e
itsjupiter <itsjupiter@gmail.com>
2013-08-04 23:51:31 +0800

    v0.035 添加重复下载

--- downagain.php
+++ downagain.php
@@ -5,9 +5,12 @@
 $user=$_SESSION['user'];
 $type=$_POST['type'];
 $filename=$_POST['filename'];
+echo $type." ".$user."你要重新下载".$filename."<br/>";
 if($type=='proofread')
-    if(getdata('proofread',$filename,'proofreader')==$user)
-        downtext("proofread",$filename);
+    {
+        if(getdata('proofread',$filename,'proofreader')==$user)
+            downtext("proofread",$filename);
+    }
 elseif($type=='polish')
     if(getdata('polish',$filename,'polisher')==$user)
         downtext("polish",$filename);
